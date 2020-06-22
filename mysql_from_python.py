6e74ae0e3e0b72ed665b74f553b444af6d12a23a
vishkid84 <vishalittr@gmail.com>
2020-06-22 22:22:46 +0000

    Added a deleted tables

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from Gitpod
@@ -12,10 +13,14 @@ connection = pymysql.connect(host="localhost",
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['jim', 'fred', 'Bob']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
+        
+        connection.commit()
 finally:
     # close the connnection
     connection.close()
